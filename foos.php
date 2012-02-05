88e5ee9b9da28110b8370b035f2075d8051489b4
Marek Ventur <marekventur@gmail.com>
2012-02-05 16:58:22 +0000

    Slightly better web view

--- foos.php
+++ foos.php
@@ -1,57 +1,124 @@
 <?php
+   //** Model **//
    require_once('foos.class.php');
 
-   $table = new FoosTable('/userhome/samuel/');
+   //** Controller **//
+   $table = new FoosTable('/home/marek/foosdata/');
    $table->loadCurrentStatus();
    $table->calculateScore();
-   $table->sortPlayers();
 
-   $tableOld = new FoosTable('/userhome/samuel/');
+   $tableOld = new FoosTable('/home/marek/foosdata/');
    $tableOld->loadStatusForTime(time() - 48 * 60 * 60);
    $tableOld->calculateScore();
-   $tableOld->sortPlayers();
 
-   echo "<h1>Foosball</h1><ol>";
 
-   $i = 1;
-   foreach ($table->getPlayers() as $player) {
-       if ($player) {
-           echo "<li><strong>".$player->getName()."</strong> ".round($player->getStrength())." in ".$player->getGames()." games ";
-           $nemesis = $player->getNemesis();
-           if ($nemesis) {
-               $nemesis = $table->getPlayerByName($nemesis);
-	       echo " (Nemesis: ".$nemesis->getName().") ";
-	   }
+   //** View **//
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Foos.fm</title>
+  <meta name="description" content="Foos.fm">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
-           $changePos = $tableOld->getPositionOfPlayer($player->getName());
-           if (!$changePos) {
-		echo "<small>NEW!</small></li>\n";
-           }
-           else
-           {
-                $changePos = $changePos - $i;
-		if ($changePos > 0) {
-	  	    echo "<small>$changePos positions improved in the last 48h</small></li>\n";
-		}
-                elseif ($changePos < 0) {
-		    $changePos *= -1;
-                    echo "<small>$changePos positions lost in the last 48h</small></li>\n";
-		}
-                else 
-                {
-		    echo "<small>No position change in the last 48h</small></li>\n";
-                }
-           }
-           $i++;
-       }
+    <table> 
+        <tr>
+            <th>48h</th>
+            <th>Position</th>
+            <th>Name</th>
+            <th>Strength</th>
+            <th>Games</th>
+            <th>Nemesis</th>
+        </tr>
+<?php
+
+    $i = 1;
+
+    /** 
+     * Columns:
+     * - Change in last 48h
+     * - Position
+     * - Name
+     * - Strength
+     * - Number of Games played
+     * - Nemesis
+     */
+
+    foreach ($table->getPlayers() as $player) {
+
+    
+        echo "<tr>";
+
+        // Change in the last 48h
+        echo "<td>";
+        $changePos = $tableOld->getPositionOfPlayer($player->getName());
+        if (!$changePos) {
+            echo "NEW!";
+        } else {
+            $changePos = $changePos - $i;
+            if ($changePos > 0) {
+                echo "+$changePos";
+            }
+            elseif ($changePos < 0) {
+                echo "$changePos";
+            } else {
+                echo "<=>";
+            }
+        }
+        echo "</td>";
+        
+        // Position   
+        echo "<td>$i.</td>";  
+        
+        // Name
+        echo "<td>".$player->getName()."</td>";
+
+        // Strength
+        echo "<td>".$player->getRoundedStrength()."</td>";
+
+        // Number of Games played
+        echo "<td>".$player->getGames()."</td>";
+
+        // Nemesis
+        echo "<td>";
+        $nemesis = $player->getNemesis();
+        if ($nemesis) {
+            echo $nemesis['player']->getName()." (+".$nemesis['count']." wins)";
+        } else {
+            echo "";
+        }
+        echo "</td>";
+
+        // Increase counter
+        $i++;
+       
    }
 
+
+?>
+    </table>
+
+<!-- LOG -->
+<?php
+   $i = 1;
+   
    echo "</ol><h2>Log</h2><ol>";
 
-   foreach ($table->getMatches() as $match) {
+   foreach (array_reverse($table->getMatches()) as $match) {
        echo "<li>";
        echo $match->getPlayer1()->getName()." (".$match->getScore1(). ") ";
        echo $match->getPlayer2()->getName()." (".$match->getScore2().") ";
        echo " <small>".date('d F Y g:i A', $match->getTimestamp())."</small></li>\n";
    }
+?>
+    
+    <!--<script src="js/scripts.js"></script>-->
+</body>
+</html>
+
+
+   
 
